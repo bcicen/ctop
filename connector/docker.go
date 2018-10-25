8a0bd3cf8a6a11b1dd6246c217a491903f92bb3e
bartfokker <bart@publicsonar.com>
2018-10-25 22:22:28 +0200

    remove unneeded cast

--- connector/docker.go
+++ connector/docker.go
@@ -105,7 +105,7 @@ func (cm *Docker) refresh(c *container.Container) {
 	c.SetMeta("created", insp.Created.Format("Mon Jan 2 15:04:05 2006"))
 	c.SetMeta("health", insp.State.Health.Status)
 	for _, env := range insp.Config.Env {
-		c.SetMeta("[ENV-VAR]", string(env))
+		c.SetMeta("[ENV-VAR]", env)
 	}
 	c.SetState(insp.State.Status)
 }
