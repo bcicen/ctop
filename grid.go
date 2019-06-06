15c5c31726705cc96f21fa69da8169903bd5dbef
Marcos Diez <marcos@unitron.com.br>
2019-06-06 09:58:55 -0300

    shows total memory usage

--- grid.go
+++ grid.go
@@ -52,6 +52,7 @@ func RedrawRows(clr bool) {
 	y := 1
 	if config.GetSwitchVal("enableHeader") {
 		header.SetCount(cursor.Len())
+		header.SetMemoryUsage(cursor.MemoryUsage())
 		header.SetFilter(config.GetVal("filterStr"))
 		y += header.Height()
 	}
