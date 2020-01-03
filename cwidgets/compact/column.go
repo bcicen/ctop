f70489821287f9b118a5c1c313e0d67684bbe485
Bradley Cicenas <bradley@vektor.nyc>
2020-01-03 13:20:31 +0000

    commit missing compact column, row files

---
+++ cwidgets/compact/column.go
@@ -0,0 +1,49 @@
+package compact
+
+import (
+	"github.com/bcicen/ctop/config"
+	"github.com/bcicen/ctop/models"
+
+	ui "github.com/gizak/termui"
+)
+
+var (
+	allCols = map[string]NewCompactColFn{
+		"status": NewStatus,
+		"name":   NewNameCol,
+		"id":     NewCIDCol,
+		"cpu":    NewCPUCol,
+		"mem":    NewMemCol,
+		"net":    NewNetCol,
+		"io":     NewIOCol,
+		"pids":   NewPIDCol,
+	}
+)
+
+type NewCompactColFn func() CompactCol
+
+func newRowWidgets() []CompactCol {
+	enabled := config.EnabledColumns()
+	cols := make([]CompactCol, len(enabled))
+
+	for n, name := range enabled {
+		wFn, ok := allCols[name]
+		if !ok {
+			panic("no such widget name: %s" + name)
+		}
+		cols[n] = wFn()
+	}
+
+	return cols
+}
+
+type CompactCol interface {
+	ui.GridBufferer
+	Reset()
+	Header() string  // header text to display for column
+	FixedWidth() int // fixed width size. if == 0, width is automatically calculated
+	Highlight()
+	UnHighlight()
+	SetMeta(models.Meta)
+	SetMetrics(models.Metrics)
+}
